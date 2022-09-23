29aaab080d3a6b9cbe7443d98932a987fd838bd5
Torbjörn Rathsman <milasudril@gmail.com>
2022-09-23 21:35:54 +0200

    Initial work on implmentation

--- lib/fdcb.cpp
+++ lib/fdcb.cpp
@@ -1,3 +1,27 @@
 //@	{"target":{"name":"fdcb.o"}}
 
 #include "./fdcb.h"
+
+struct fdcb_context
+{
+	explicit fdcb_context(int fd, fdcb_callback callback, void* user_context)
+	{}
+
+	void flush()
+	{}
+};
+
+struct fdcb_context* fdcb_create_context(int fd, fdcb_callback callback, void* user_context)
+{
+	return new fdcb_context{fd, callback, user_context};
+}
+
+void fdcb_flush(struct fdcb_context* ctxt)
+{
+	ctxt->flush();
+}
+
+void fdcb_free_context(fdcb_context* ctxt)
+{
+	delete ctxt;
+}
